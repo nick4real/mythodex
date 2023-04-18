6d6233a4fab7bc3fb5c6b2371b38adbda9b59b5f
nick4real <67540024+nick4real@users.noreply.github.com>
2023-04-19 00:20:29 +0300

    MainWindow, files refactoring

--- Mythodex/MainWindow.xaml.cs
+++ Mythodex/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Interactivity;
+using Mythodex.View;
 using Mythodex.ViewModel;
 
 namespace Mythodex
@@ -7,13 +8,14 @@ namespace Mythodex
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class ViewMain : Window
+    public partial class MainWindow : Window
     {
-        public ViewMain()
+        public MainWindow()
         {
             InitializeComponent();
 
             DataContext = new ViewModelMain();
+            MainPanelPage.Navigate(new Today());
         }
     }
 }
---
+++ Mythodex/View/Today.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using Mythodex.View;
+using Mythodex.ViewModel;
+
+namespace Mythodex.View
+{
+    /// <summary>
+    /// Interaction logic for Today.xaml
+    /// </summary>
+    public partial class Today : Page
+    {
+        public Today()
+        {
+            InitializeComponent();
+
+            DataContext = new TodayViewModel();
+        }
+    }
+}
